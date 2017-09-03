7ddfb28e64b1f2b6ba0addccd0f6bb2fa0ba3313
steadmankondor <steadmankondor@users.noreply.github.com>
2017-09-03 14:49:36 +1000

    Today's work
    Today's work

---
+++ src/main/java/com/ivansun/TestArrayWriter/ArrayWriter.java
@@ -0,0 +1,33 @@
+// a new for git.
+// http://opencsv.sourceforge.net/apidocs/com/opencsv/CSVWriter.html
+
+package com.ivansun.TestArrayWriter;
+
+import com.opencsv.CSVWriter;
+import java.io.*;
+import java.util.Arrays;
+
+public class ArrayWriter {
+	public static void main(String arg[]){
+	    CSVWriter writer;
+		try {
+			writer = new CSVWriter(new FileWriter("yourfile.csv"));
+		    // feed in your array (or convert your data to an array)
+		    String[] entries = "first#second#third".split("#");
+		    writer.writeNext(entries);
+		    String[] entries2 = "first2#second2#third3".split("#");
+		    writer.writeNext(entries2);
+		    
+		    writer.close();
+		    System.out.println(Arrays.toString(entries2));	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		
+		
+	}
+ 
+	
+}
